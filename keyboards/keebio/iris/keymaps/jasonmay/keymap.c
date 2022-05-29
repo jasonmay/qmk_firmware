e962b6fcbc718eb42daaf27c0faabfbfcadc7cfe
Jason May <jason.a.may@gmail.com>
2022-05-29 17:35:27 -0600

    some more keymap changes, improve space cadet

--- keyboards/keebio/iris/keymaps/jasonmay/keymap.c
+++ keyboards/keebio/iris/keymaps/jasonmay/keymap.c
@@ -9,10 +9,14 @@
 #define XX_ESC LCTL_T(KC_ESC)
 
 // 1=], 2=lower
-#define XX_LEN LT(_LOWER, KC_ENT)
+#define XX_LJM LT(_LOWER, KC_Z)
 
 // 1=[, 2=lower
-#define XX_RSP LT(_RAISE, KC_SPC)
+// #define XX_RTAB LT(_RAISE, KC_TAB)
+#define XX_RTAB LSFT_T(KC_TAB)
+
+// expose on my mac
+#define XX_XPOS LCTL(KC_UP)
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
@@ -21,19 +25,23 @@ enum custom_keycodes {
   JMSEQ, // sequences for jason may
 };
 
+enum td_keycodes {
+  LEFT_SHIFT_PAREN
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      KC_EQL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
+     XX_RTAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      XX_ESC,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC,          KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC,
+     KC_LSPO,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC,          KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,KC_RSPC,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    KC_LGUI, XX_LEN,   JMSEQ,                    KC_BSPC, XX_RSP,   KC_RALT
+                                    KC_LGUI, KC_ENT,  XX_LJM,                    KC_BSPC,  KC_SPC,  KC_RALT
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
@@ -41,13 +49,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                            KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     RESET,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
+     RESET,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR,    _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_DEL,  _______, _______, _______, _______, _______,                            KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,KC_PLUS, KC_PIPE,
+     KC_DEL,  _______, _______, _______, _______, _______,                            KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,_______, KC_PIPE,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     BL_STEP, _______, _______, _______, _______, _______, KC_F3,            KC_F4,   _______, KC_P1,   KC_P2,   KC_P3,   KC_MINS, _______,
-  //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    _______, _______, _______,                   _______, _______, KC_P0
+     BL_STEP, _______, _______, _______, _______, _______, KC_F3,            XX_XPOS, _______, KC_LCBR, KC_RCBR, _______, _______, _______,
+  //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴─── ─────┘
+                                    _______, _______, _______,                   KC_LCBR, KC_RCBR, KC_P0
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
@@ -55,11 +63,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                              KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     RGB_TOG, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                            KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
+     _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                            KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      RGB_MOD, KC_MPRV, KC_MNXT, KC_VOLU, KC_PGUP, KC_LCBR,                            KC_RCBR,  KC_HOME, RGB_HUI, RGB_SAI, RGB_VAI, KC_BSLS,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_MUTE, KC_MSTP, KC_MPLY, KC_VOLD, KC_PGDN, KC_LBRC, _______,          _______, KC_RBRC, KC_END,  RGB_HUD, RGB_SAD, RGB_VAD, _______,
+     KC_MUTE, KC_MSTP, KC_MPLY, KC_VOLD, KC_PGDN, KC_LBRC, _______,          RGB_TOG, KC_RBRC, KC_END,  RGB_HUD, RGB_SAD, RGB_VAD, _______,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     _______, _______, _______,                   _______, _______, _______
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
@@ -67,10 +75,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  uint16_t lshift = (get_mods() & MOD_BIT(KC_LSFT));
+  uint16_t rshift = (get_mods() & MOD_BIT(KC_RSFT));
 #ifdef CONSOLE_ENABLE
-    uprintf("PRU: %u, pressed: %u, interrupted: %u, count: %u\n", keycode, record->event.pressed, record->tap.interrupted, record->tap.count);
+  uprintf("PRU: %u, pressed: %u, i:%u LS:%u, RS:%u\n", keycode, record->event.pressed, record->tap.interrupted, lshift, rshift);
 #endif
+
   switch (keycode) {
+    // make rolling )( work again
+    case KC_LSPO:
+      if (record->event.pressed && rshift == MOD_BIT(KC_RSFT)) {
+        tap_code(KC_0);
+      }
+      break;
+    // make rolling () work again
+    case KC_RSPC:
+      if (record->event.pressed && lshift == MOD_BIT(KC_LSFT)) {
+        tap_code(KC_9);
+      }
+      break;
     case QWERTY:
       if (record->event.pressed) {
         set_single_persistent_default_layer(_QWERTY);
@@ -104,13 +127,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 }
 
 bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case XX_LEN:
-        case XX_RSP:
-            // Immediately select the hold action when another key is tapped.
-            return true;
-        default:
-            // Do not select the hold action when another key is tapped.
-            return false;
+  switch (keycode) {
+    case XX_LJM:
+    case XX_ESC:
+    case KC_LSFT:
+      return true;
+    default:
+        return false;
     }
 }
