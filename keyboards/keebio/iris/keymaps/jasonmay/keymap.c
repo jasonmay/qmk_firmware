d707bfeca82c596762850d89222a63b9d600624f
Jason May <jason.a.may@gmail.com>
2022-05-29 11:13:01 -0600

    console debug message

--- keyboards/keebio/iris/keymaps/jasonmay/keymap.c
+++ keyboards/keebio/iris/keymaps/jasonmay/keymap.c
@@ -67,6 +67,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+#ifdef CONSOLE_ENABLE
+    uprintf("PRU: %u, pressed: %u, interrupted: %u, count: %u\n", keycode, record->event.pressed, record->tap.interrupted, record->tap.count);
+#endif
   switch (keycode) {
     case QWERTY:
       if (record->event.pressed) {
