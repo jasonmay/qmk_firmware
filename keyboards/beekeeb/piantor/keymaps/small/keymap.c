e89187f2cc318fdb34464108a88ff964500c061f
Jason May <jason.a.may@gmail.com>
2023-10-26 09:12:38 -0600

    move usual macros into combos

--- keyboards/beekeeb/piantor/keymaps/small/keymap.c
+++ keyboards/beekeeb/piantor/keymaps/small/keymap.c
@@ -3,29 +3,6 @@
 
 #include QMK_KEYBOARD_H
 
-// const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-//      /*
-//       * ┌───┬───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┬───┐
-//       * │Tab│ Q │ W │ E │ R │ T │       │ Y │ U │ I │ O │ P │Bsp│
-//       * ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
-//       * │Ctl│ A │ S │ D │ F │ G │       │ H │ J │ K │ L │ ; │ ' │
-//       * ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
-//       * │Sft│ Z │ X │ C │ V │ B │       │ N │ M │ , │ . │ / │Sft│
-//       * └───┴───┴───┴───┴───┴───┘       └───┴───┴───┴───┴───┴───┘
-//       *               ┌───┐                   ┌───┐
-//       *               │GUI├───┐           ┌───┤Alt│
-//       *               └───┤Bsp├───┐   ┌───┤Ent├───┘
-//       *                   └───┤   │   │   ├───┘
-//       *                       └───┘   └───┘
-//       */
-//     [0] = LAYOUT_split_3x6_3(
-//         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-//         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-//         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-//                                             KC_LGUI, KC_BSPC, KC_SPC,           KC_SPC,  KC_ENT,  KC_RALT
-//     )
-// };
-
 // #include "config.h"
 // #include "quantum.h"
 // #include "action.h"
@@ -43,8 +20,8 @@
 #define XX_ESC LCTL_T(KC_ESC)
 
 // 1=vimcmd, 2=lower
-#define XX_LJM LT(_LOWER, JMSEQ)
-
+// #define XX_LJM JMSEQ
+#define XX_LJM MO(_LOWER)
 
 // 1=tab, 2=raise
 #define XX_RTAB LT(_RAISE, KC_TAB)
@@ -104,86 +81,105 @@ enum custom_keycodes {
   QWERTY = SAFE_RANGE,
   LOWER,
   RAISE,
-  JMSEQ, // sequence for jason may (for env-determined vim dev stuff)
+  JMSEQ,
 };
 
+const uint16_t PROGMEM combo1[] = {KC_J, KC_K, KC_A, COMBO_END};
+const uint16_t PROGMEM combo2[] = {KC_J, KC_K, KC_S, COMBO_END};
+const uint16_t PROGMEM combo3[] = {KC_J, KC_K, KC_D, COMBO_END};
+const uint16_t PROGMEM combo4[] = {KC_J, KC_K, KC_F, COMBO_END};
+const uint16_t PROGMEM combo5[] = {KC_J, KC_K, KC_G, COMBO_END};
+const uint16_t PROGMEM combo6[] = {KC_D, KC_F, KC_H, COMBO_END};
+const uint16_t PROGMEM combo7[] = {KC_D, KC_F, KC_J, COMBO_END};
+const uint16_t PROGMEM combo8[] = {KC_D, KC_F, KC_K, COMBO_END};
+const uint16_t PROGMEM combo9[] = {KC_D, KC_F, KC_L, COMBO_END};
+const uint16_t PROGMEM combo0[] = {KC_D, KC_F, KC_SCLN, COMBO_END};
 
-const uint16_t PROGMEM test_combo1[] = {KC_J, KC_K, KC_A, COMBO_END};
-const uint16_t PROGMEM test_combo2[] = {KC_J, KC_K, KC_S, COMBO_END};
-const uint16_t PROGMEM test_combo3[] = {KC_J, KC_K, KC_D, COMBO_END};
-const uint16_t PROGMEM test_combo4[] = {KC_J, KC_K, KC_F, COMBO_END};
-const uint16_t PROGMEM test_combo5[] = {KC_J, KC_K, KC_G, COMBO_END};
-const uint16_t PROGMEM test_combo6[] = {KC_D, KC_F, KC_H, COMBO_END};
-const uint16_t PROGMEM test_combo7[] = {KC_D, KC_F, KC_J, COMBO_END};
-const uint16_t PROGMEM test_combo8[] = {KC_D, KC_F, KC_K, COMBO_END};
-const uint16_t PROGMEM test_combo9[] = {KC_D, KC_F, KC_L, COMBO_END};
-const uint16_t PROGMEM test_combo0[] = {KC_D, KC_F, KC_SCLN, COMBO_END};
-
-const uint16_t PROGMEM test_combo_s1[] = {KC_J, KC_K, KC_L, KC_A, COMBO_END};
-const uint16_t PROGMEM test_combo_s2[] = {KC_J, KC_K, KC_L, KC_S, COMBO_END};
-const uint16_t PROGMEM test_combo_s3[] = {KC_J, KC_K, KC_L, KC_D, COMBO_END};
-const uint16_t PROGMEM test_combo_s4[] = {KC_J, KC_K, KC_L, KC_F, COMBO_END};
-const uint16_t PROGMEM test_combo_s5[] = {KC_J, KC_K, KC_L, KC_G, COMBO_END};
-const uint16_t PROGMEM test_combo_s6[] = {KC_S, KC_D, KC_F, KC_H, COMBO_END};
-const uint16_t PROGMEM test_combo_s7[] = {KC_S, KC_D, KC_F, KC_J, COMBO_END};
-const uint16_t PROGMEM test_combo_s8[] = {KC_S, KC_D, KC_F, KC_K, COMBO_END};
-const uint16_t PROGMEM test_combo_s9[] = {KC_S, KC_D, KC_F, KC_L, COMBO_END};
-const uint16_t PROGMEM test_combo_s0[] = {KC_S, KC_D, KC_F, KC_SCLN, COMBO_END};
-const uint16_t PROGMEM test_combo_sa[] = {KC_S, KC_D, KC_F, KC_QUOT, COMBO_END};
+const uint16_t PROGMEM combo_s1[] = {KC_J, KC_K, KC_L, KC_A, COMBO_END};
+const uint16_t PROGMEM combo_s2[] = {KC_J, KC_K, KC_L, KC_S, COMBO_END};
+const uint16_t PROGMEM combo_s3[] = {KC_J, KC_K, KC_L, KC_D, COMBO_END};
+const uint16_t PROGMEM combo_s4[] = {KC_J, KC_K, KC_L, KC_F, COMBO_END};
+const uint16_t PROGMEM combo_s5[] = {KC_J, KC_K, KC_L, KC_G, COMBO_END};
+const uint16_t PROGMEM combo_s6[] = {KC_S, KC_D, KC_F, KC_H, COMBO_END};
+const uint16_t PROGMEM combo_s7[] = {KC_S, KC_D, KC_F, KC_J, COMBO_END};
+const uint16_t PROGMEM combo_s8[] = {KC_S, KC_D, KC_F, KC_K, COMBO_END};
+const uint16_t PROGMEM combo_s9[] = {KC_S, KC_D, KC_F, KC_L, COMBO_END};
+const uint16_t PROGMEM combo_s0[] = {KC_S, KC_D, KC_F, KC_SCLN, COMBO_END};
+const uint16_t PROGMEM combo_sa[] = {KC_S, KC_D, KC_F, KC_QUOT, COMBO_END};
 
 // KC_TAB eventually. just make it work for now so we have QK_BOOT
-const uint16_t PROGMEM test_combo_eql[] = {KC_J, KC_K, XX_RTAB, COMBO_END};
-const uint16_t PROGMEM test_combo_plus[] = {KC_J, KC_K, XX_ESC, COMBO_END};
-const uint16_t PROGMEM test_combo_mins[] = {KC_D, KC_F, KC_QUOT, COMBO_END};
-const uint16_t PROGMEM test_combo_under[] = {KC_F, KC_J, COMBO_END};
+const uint16_t PROGMEM combo_eql[] = {KC_J, KC_K, XX_RTAB, COMBO_END};
+const uint16_t PROGMEM combo_plus[] = {KC_J, KC_K, XX_ESC, COMBO_END};
+const uint16_t PROGMEM combo_mins[] = {KC_D, KC_F, KC_QUOT, COMBO_END};
+const uint16_t PROGMEM combo_under[] = {KC_F, KC_J, COMBO_END};
 
-const uint16_t PROGMEM test_combo_g1[] = {KC_L, KC_K, KC_SCLN, KC_A, COMBO_END};
-const uint16_t PROGMEM test_combo_g2[] = {KC_L, KC_K, KC_SCLN, KC_S, COMBO_END};
-const uint16_t PROGMEM test_combo_g3[] = {KC_L, KC_K, KC_SCLN, KC_D, COMBO_END};
-const uint16_t PROGMEM test_combo_g4[] = {KC_L, KC_K, KC_SCLN, KC_F, COMBO_END};
-const uint16_t PROGMEM test_combo_g5[] = {KC_L, KC_K, KC_SCLN, KC_G, COMBO_END};
-const uint16_t PROGMEM test_combo_g6[] = {KC_A, KC_S, KC_D, KC_H, COMBO_END};
-const uint16_t PROGMEM test_combo_g7[] = {KC_A, KC_S, KC_D, KC_J, COMBO_END};
-const uint16_t PROGMEM test_combo_g8[] = {KC_A, KC_S, KC_D, KC_K, COMBO_END};
-const uint16_t PROGMEM test_combo_g9[] = {KC_A, KC_S, KC_D, KC_L, COMBO_END};
-const uint16_t PROGMEM test_combo_g0[] = {KC_A, KC_S, KC_D, KC_SCLN, COMBO_END};
+const uint16_t PROGMEM combo_g1[] = {KC_L, KC_K, KC_SCLN, KC_A, COMBO_END};
+const uint16_t PROGMEM combo_g2[] = {KC_L, KC_K, KC_SCLN, KC_S, COMBO_END};
+const uint16_t PROGMEM combo_g3[] = {KC_L, KC_K, KC_SCLN, KC_D, COMBO_END};
+const uint16_t PROGMEM combo_g4[] = {KC_L, KC_K, KC_SCLN, KC_F, COMBO_END};
+const uint16_t PROGMEM combo_g5[] = {KC_L, KC_K, KC_SCLN, KC_G, COMBO_END};
+const uint16_t PROGMEM combo_g6[] = {KC_A, KC_S, KC_D, KC_H, COMBO_END};
+const uint16_t PROGMEM combo_g7[] = {KC_A, KC_S, KC_D, KC_J, COMBO_END};
+const uint16_t PROGMEM combo_g8[] = {KC_A, KC_S, KC_D, KC_K, COMBO_END};
+const uint16_t PROGMEM combo_g9[] = {KC_A, KC_S, KC_D, KC_L, COMBO_END};
+const uint16_t PROGMEM combo_g0[] = {KC_A, KC_S, KC_D, KC_SCLN, COMBO_END};
+
+const uint16_t PROGMEM combo_left_space[] = {KC_Q, KC_W, KC_Y, COMBO_END};
+const uint16_t PROGMEM combo_right_space[] = {KC_R, KC_T, KC_Y, COMBO_END};
+
+const uint16_t PROGMEM combo_seq[] = {KC_J, KC_K, KC_L, COMBO_END};
+const uint16_t PROGMEM combo_seq2[] = {KC_H, KC_J, KC_K, COMBO_END};
+
+const uint16_t PROGMEM combo_fscreen[] = {KC_E, KC_R, KC_U, KC_I, COMBO_END};
+const uint16_t PROGMEM combo_lscreen[] = {KC_W, KC_E, KC_R, COMBO_END};
+const uint16_t PROGMEM combo_rscreen[] = {KC_U, KC_I, KC_O, COMBO_END};
 
 combo_t key_combos[] = {
-    COMBO(test_combo1, KC_1),
-    COMBO(test_combo2, KC_2),
-    COMBO(test_combo3, KC_3),
-    COMBO(test_combo4, KC_4),
-    COMBO(test_combo5, KC_5),
-    COMBO(test_combo6, KC_6),
-    COMBO(test_combo7, KC_7),
-    COMBO(test_combo8, KC_8),
-    COMBO(test_combo9, KC_9),
-    COMBO(test_combo0, KC_0),
-    COMBO(test_combo_s1, KC_EXLM),
-    COMBO(test_combo_s2, KC_AT),
-    COMBO(test_combo_s3, KC_HASH),
-    COMBO(test_combo_s4, KC_DLR),
-    COMBO(test_combo_s5, KC_PERC),
-    COMBO(test_combo_s6, KC_CIRC),
-    COMBO(test_combo_s7, KC_AMPR),
-    COMBO(test_combo_s8, KC_ASTR),
-    COMBO(test_combo_s9, KC_GRV),
-    COMBO(test_combo_s0, KC_TILD),
-    COMBO(test_combo_g1, LGUI(KC_1)),
-    COMBO(test_combo_g2, LGUI(KC_2)),
-    COMBO(test_combo_g3, LGUI(KC_3)),
-    COMBO(test_combo_g4, LGUI(KC_4)),
-    COMBO(test_combo_g5, LGUI(KC_5)),
-    COMBO(test_combo_g6, LGUI(KC_6)),
-    COMBO(test_combo_g7, LGUI(KC_7)),
-    COMBO(test_combo_g8, LGUI(KC_8)),
-    COMBO(test_combo_g9, LGUI(KC_9)),
-    COMBO(test_combo_g0, LGUI(KC_0)),
+    COMBO(combo1, KC_1),
+    COMBO(combo2, KC_2),
+    COMBO(combo3, KC_3),
+    COMBO(combo4, KC_4),
+    COMBO(combo5, KC_5),
+    COMBO(combo6, KC_6),
+    COMBO(combo7, KC_7),
+    COMBO(combo8, KC_8),
+    COMBO(combo9, KC_9),
+    COMBO(combo0, KC_0),
+    COMBO(combo_s1, KC_EXLM),
+    COMBO(combo_s2, KC_AT),
+    COMBO(combo_s3, KC_HASH),
+    COMBO(combo_s4, KC_DLR),
+    COMBO(combo_s5, KC_PERC),
+    COMBO(combo_s6, KC_CIRC),
+    COMBO(combo_s7, KC_AMPR),
+    COMBO(combo_s8, KC_ASTR),
+    COMBO(combo_s9, KC_GRV),
+    COMBO(combo_s0, KC_TILD),
+    COMBO(combo_g1, LGUI(KC_1)),
+    COMBO(combo_g2, LGUI(KC_2)),
+    COMBO(combo_g3, LGUI(KC_3)),
+    COMBO(combo_g4, LGUI(KC_4)),
+    COMBO(combo_g5, LGUI(KC_5)),
+    COMBO(combo_g6, LGUI(KC_6)),
+    COMBO(combo_g7, LGUI(KC_7)),
+    COMBO(combo_g8, LGUI(KC_8)),
+    COMBO(combo_g9, LGUI(KC_9)),
+    COMBO(combo_g0, LGUI(KC_0)),
 
-    COMBO(test_combo_plus, KC_PLUS),
-    COMBO(test_combo_mins, KC_MINS),
-    COMBO(test_combo_eql, KC_EQL),
-    COMBO(test_combo_under, KC_UNDS),
+    COMBO(combo_plus, KC_PLUS),
+    COMBO(combo_mins, KC_MINS),
+    COMBO(combo_eql, KC_EQL),
+    COMBO(combo_under, KC_UNDS),
+
+    COMBO(combo_left_space, LALT(KC_LBRC)),
+    COMBO(combo_right_space, LALT(KC_RBRC)),
+
+    COMBO(combo_seq, JMSEQ),
+    COMBO(combo_seq2, JMSEQ),
+
+    COMBO(combo_fscreen, XX_FWIN),
+    COMBO(combo_lscreen, XX_LWIN),
+    COMBO(combo_rscreen, XX_RWIN),
 };
 
 
@@ -191,12 +187,14 @@ combo_t key_combos[] = {
 #define rshift (get_mods() & MOD_BIT(KC_RSFT))
 
 
-bool process_record_keymap(uint16_t keycode, keyrecord_t *record) {
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   // for second press of mod/layer tap combo
-  bool chorded = (record->tap.count && record->event.pressed);
+  // bool chorded = (record->tap.count && record->event.pressed);
   static bool lgui_pressed = false;
   static bool lgui_interrupted = false;
   bool reset_lgui_interrupted = false;
+
+  uprintf("keycode: %u\n", keycode);
 
   if (lgui_pressed && record->event.pressed) {
     if (keycode != XX_ABRC) {
@@ -205,7 +203,7 @@ bool process_record_keymap(uint16_t keycode, keyrecord_t *record) {
   }
 
   if (!lgui_pressed && !record->event.pressed) {
-      unregister_code(KC_LGUI);
+    unregister_code(KC_LGUI);
   }
   if (lgui_interrupted && keycode != XX_CBRC && !record->event.pressed) {
     reset_lgui_interrupted = true;
@@ -263,11 +261,17 @@ bool process_record_keymap(uint16_t keycode, keyrecord_t *record) {
       }
       return false;
       break;
-    case XX_LJM:
-      if (chorded) {
-        SEND_STRING("\\ks");
-        return false;
+    case JMSEQ:
+      if (record->event.pressed) {
+        register_code(KC_BSLS);
+        register_code(KC_K);
+        register_code(KC_S);
+      } else {
+        unregister_code(KC_BSLS);
+        unregister_code(KC_K);
+        unregister_code(KC_S);
       }
+      return false;
       break;
   }
   return true;
@@ -283,28 +287,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_LOWER] = LAYOUT_split_3x6_3(
-  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      XX_LWIN,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  XX_RWIN,
-  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_DEL,  XX_LWKS, KC_HOME, _______, XX_XPOS, _______,                            KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,XX_RWKS, KC_PIPE,
-  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_GRV,  _______, KC_END, _______, _______,  XX_BBK,                             XX_BFD,  KC_LCBR, KC_RCBR, _______, _______, QK_BOOT,
-  //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴─── ─────┘
                                     XY_LTO,  XY_LTM,  XY_LTI,                    XY_RTI,  XY_RTM,  XY_RTO
-                                // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
-
   [_RAISE] = LAYOUT_split_3x6_3(
-  //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                            KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
-  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├───H────┼───J────┼───K────┼───L────┼───;────┼────────┤
-     RGB_MOD, KC_MPRV, KC_MNXT, KC_VOLU, KC_PGUP, _______,                         _______, _______, RGB_HUI, RGB_SAI, RGB_VAI, KC_BSLS,
-  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+     RGB_MOD, KC_MPRV, KC_MNXT, KC_VOLU, KC_PGUP, _______,                            _______, _______, RGB_HUI, RGB_SAI, RGB_VAI, KC_BSLS,
      KC_MUTE, KC_MSTP, KC_MPLY, KC_VOLD, KC_PGDN, _______,                            _______, _______, RGB_HUD, RGB_SAD, RGB_VAD, _______,
-  //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     _______, _______, _______,                   _______, _______, QK_BOOT
-                                // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 };
 
